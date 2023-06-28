8fc24d23ec95fb992f2298a078c5adf23ae454b5
SamiraHolanda <137845751+SamiraHolanda@users.noreply.github.com>
2023-06-28 14:57:43 -0300

    Atividade - Aula 02
    28/06/2023

---
+++ Aula02.js
@@ -0,0 +1,27 @@
+//Questão 1:
+var num = new Number(300);
+console.log(num);
+
+var num1 =  new Number(999);
+console.log(num1);
+
+var num2 = new Number(21);
+console.log(num2);
+
+//Questão 2:
+Number.MAX_VALUE;
+console.log(Number.MAX_VALUE);
+
+Number.MAX_SAFE_INTEGER;
+console.log(Number.MAX_SAFE_INTEGER);
+
+Number.MIN_VALUE;
+console.log(Number.MIN_VALUE);
+
+Number.MIN_SAFE_INTEGER;
+console.log(Number.MIN_SAFE_INTEGER);
+
+//Questão 3:
+var valor = new Number (150);
+console.log(valor);
+console.log(valor.toString());
